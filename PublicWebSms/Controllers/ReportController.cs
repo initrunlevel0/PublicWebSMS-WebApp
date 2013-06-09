23d61ba64ae9db87821d58fb6bd76cfc6f92ef65
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2013-06-09 10:43:55 +0700

    Added completed (need testing) implementation of ReportController

--- PublicWebSms/Controllers/ReportController.cs
+++ PublicWebSms/Controllers/ReportController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PublicWebSms.Models;
+using System.Configuration;
 
 namespace PublicWebSms.Controllers
 {
@@ -14,13 +15,30 @@ namespace PublicWebSms.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            ReportData report = new ReportData();
+
+            try
+            {
+                report = ReportData.GetCurrentReportData();
+            }
+            catch
+            {
+                return View("Error");
+            }
+
+            return View(report);
+            
         }
 
         [HttpPost]
         public ActionResult ReportAPI(ReportData reportData, SMSAPIInput apiData)
         {
-
+            // Process wtih some secret data
+            if (apiData.APIId == "andnowforsomethingcompletlydifferent" && apiData.APISecretCode == "it's")
+            {
+                ConfigurationManager.AppSettings["ServerLastUpdate"] = reportData.LastUpdate.ToString();
+                ConfigurationManager.AppSettings["ServerString"] = reportData.ServerString;
+            }
             return Json(null);
         }
 
--- PublicWebSms/Models/ReportData.cs
+++ PublicWebSms/Models/ReportData.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 
 namespace PublicWebSms.Models
 {
     public class ReportData
     {
-        public bool ServerOn { get; set; }
-        public string ServerName { get; set; }
-        public string ServerPosition { get; set; }
-        public string GammuInfo { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string ServerString { get; set; }
+        public bool IsOn()
+        {
+            TimeSpan tressholdTime = LastUpdate - DateTime.Now;
+            if (tressholdTime.Minutes > 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static ReportData GetCurrentReportData()
+        {
+            ReportData report = new ReportData();
+            report.LastUpdate = DateTime.Parse(ConfigurationManager.AppSettings["ServerLastUpdate"]);
+            report.ServerString = ConfigurationManager.AppSettings["ServerString"];
+            return report;
+        }
     }
 }
