9d7ed3d32a18dc49b74cdf7c67e9a7df58230bb1
Putu Wiramaswara Widya <initrunlevel0@gmail.com>
2013-06-09 09:48:32 +0700

    Initial implementation of Server reporting

---
+++ PublicWebSms/Controllers/ReportController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PublicWebSms.Models;
+
+namespace PublicWebSms.Controllers
+{
+    public class ReportController : Controller
+    {
+        //
+        // GET: /Report/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ReportAPI(ReportData reportData, SMSAPIInput apiData)
+        {
+
+            return Json(null);
+        }
+
+    }
+}
---
+++ PublicWebSms/Models/ReportData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PublicWebSms.Models
+{
+    public class ReportData
+    {
+        public bool ServerOn { get; set; }
+        public string ServerName { get; set; }
+        public string ServerPosition { get; set; }
+        public string GammuInfo { get; set; }
+    }
+}
